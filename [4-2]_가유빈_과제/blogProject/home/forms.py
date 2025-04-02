b5ff2b3bd182f3e25f2db63a99d2c87d85e135e5
kayubin <kayubin@gmail.com>
2025-04-02 20:26:13 +0900

    [4-2]_가유빈_과제

---
+++ [4-2]_가유빈_과제/blogProject/home/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from home.models import Post, Comment
+
+class PostModelForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'photo']  # 입력받을 필드를 정의
+
+class CommentForm(forms.ModelForm):
+    class Meta: 
+        model = Comment 
+        fields = ['comment']
