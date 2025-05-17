47842c8f2c60148c346b9121c059fc7a07ac61ba
zuyo7852 <leejuyeon7852@gmail.com>
2025-05-17 18:28:41 +0900

    [7-1]_이주연_과제

---
+++ [7-1]_이주연_과제/DRFPracticeProject/accounts/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from .serilalizers import UserSerializer
+from rest_framework import viewsets
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
