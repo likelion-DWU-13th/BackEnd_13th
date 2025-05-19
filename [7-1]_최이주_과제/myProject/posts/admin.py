4c5900c1c96ec491811982738ac88f10600b7079
yijoo <761610@naver.com>
2025-05-19 12:29:50 +0900

    [7-1]_최이주_과제

---
+++ [7-1]_최이주_과제/myProject/posts/admin.py
@@ -0,0 +1,7 @@
+# postboxProject / posts / admin.py ( posts 앱 내 admin.py ) 
+
+from django.contrib import admin
+from .models import Post, Comment
+
+admin.site.register(Post)
+admin.site.register(Comment)
